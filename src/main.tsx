44f062c78fd959a0d34a745e487c9497eededb1e
Alberto <rescignoalbe@gmail.com>
2025-02-02 12:55:34 +0000

    Added BrowserRouter to fix routing issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/react'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
-import App from './App.tsx'
-import './index.css'
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Analytics />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+      <Analytics />
+    </BrowserRouter>
+  </StrictMode>
+);
